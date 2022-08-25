f62980117d598993ff57aab74bae2be80c28e38f
pavan <pavanambulkar69@gmail.com>
2022-08-26 00:09:00 +0530

    plan

--- src/Pages/Enterprise.jsx
+++ src/Pages/Enterprise.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Accelerate from '../EnterpriseCompo/Accelerate'
 import Logos from '../EnterpriseCompo/Logos'
+import Plans from '../EnterpriseCompo/Plans'
 
 const Enterprise = () => {
   return (
     <div>
         <Accelerate/>
         <Logos/>
+        <Plans/>
     </div>
   )
 }
