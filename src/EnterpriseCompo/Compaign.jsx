2ddcb0030ddf6804a08e01637d64bf08c0795b8e
pavan <pavanambulkar69@gmail.com>
2022-08-26 02:04:22 +0530

    save

--- src/EnterpriseCompo/Compaign.jsx
+++ src/EnterpriseCompo/Compaign.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React from "react";
 import { Box, Text, Img, Button, Flex, Wrap, WrapItem } from "@chakra-ui/react";
 
 const Compaign = () => {
   return (
     <>
-    <Box className="b5" fontSize={["sm", "md", "lg", "xl"]}>
-    <Text className="takeoff">
-    Best in-class features for best-in-class campaigns
+      <Box className="b5" fontSize={["sm", "md", "lg", "xl"]}>
+        <Text className="takeoff">
+          Best in-class features for best-in-class campaigns
+        </Text>
+        <Text width="90%" margin="auto">
+          Automation without tech support? Yes, we can! APIs to make
+          integrations easy? Check! Multi-account management and Single-Sign-On?
+          Of course! Get a tour of our user-friendly interface and get to know
+          all our advanced capabilities.
+        </Text>
+      </Box>
 
-    </Text>
-    <Text width= '90%' margin='auto'>
-    Automation without tech support? Yes, we can! APIs to make integrations easy? Check! Multi-account management and Single-Sign-On? Of course! Get a tour of our user-friendly interface and get to know all our advanced capabilities.
-    </Text>
-  </Box>
-
-  <Flex width='96%' paddingTop='40px' margin='auto' color="white" className="f2">
-        
-        <Box flex="1" >
-            <Text fontSize="23px" color='orange'>CAMPAIGN MANAGEMENT</Text>
-          <Text className="tak">Emails and SMS, easily designed and  delivered</Text>
-          <Text color='grey' fontSize='18px' >
-          We offer a unique drag and drop interface that is so user-friendly, you’ll be excited about your next campaign! Make it count even more with personalized landing pages created in a few clicks. We’re deliverability experts, so we know how to land your campaigns where they belong: in your clients’ inbox. Automation also becomes a breeze with our flexible workflow manager.
+      <Flex
+        width="96%"
+        paddingTop="40px"
+        margin="auto"
+        color="white"
+        className="f2"
+      >
+        <Box flex="1">
+          <Text fontSize="23px" color="orange">
+            CAMPAIGN MANAGEMENT
           </Text>
-          <Button marginTop='10px' colorScheme='orange' variant='link'>
-          Learn more about campaign management
-  </Button>
+          <Text className="tak">
+            Emails and SMS, easily designed and delivered
+          </Text>
+          <Text color="grey" fontSize="18px">
+            We offer a unique drag and drop interface that is so user-friendly,
+            you’ll be excited about your next campaign! Make it count even more
+            with personalized landing pages created in a few clicks. We’re
+            deliverability experts, so we know how to land your campaigns where
+            they belong: in your clients’ inbox. Automation also becomes a
+            breeze with our flexible workflow manager.
+          </Text>
+          <Button marginTop="10px" colorScheme="orange" variant="link">
+            Learn more about campaign management
+          </Button>
         </Box>
-        <Box flex="1" >
+        <Box flex="1">
           <Img
             margin="auto"
             height="350px"
@@ -36,7 +52,7 @@ const Compaign = () => {
         </Box>
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default Compaign
+export default Compaign;
